a89fc500e0b3182c002ac3663beac4fcd7009d54
Vinayak Goyal <73058928+ivinayakg@users.noreply.github.com>
2021-08-21 06:05:05 +0530

    styling completed structuring and styling of the app is completed.

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# Profit-Loss-calculator
-Created with CodeSandbox
+# Profit Loss calculator
+
+## Its a very simple React app to calculate your profit or loss on any share that you have bought.
+
+### created with code sandbox
--- src/App.js
+++ src/App.js
@@ -1,10 +1,34 @@
 import "./styles.css";
+import Footer from "./Footer";
+import { useState } from "react";
 
 export default function App() {
+  const [msg, setMsg] = useState("");
+
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <div className="section">
+        <div className="contain">
+          <h1>Profit Loss Calculator</h1>
+          <div className="inputs">
+            <label>
+              Initial Price:
+              <input type="number" name="Initial Price" />
+            </label>
+            <label>
+              Current Price:
+              <input type="number" name="Current Price" />
+            </label>
+            <label>
+              Quantity:
+              <input type="number" name="Quantity" />
+            </label>
+            <button className="btn">Calculate</button>
+          </div>
+          <h4>{msg}</h4>
+        </div>
+      </div>
+      <Footer />
     </div>
   );
 }
