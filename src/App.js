d14a3dc8967cfe7b68f422f117283063ee67afd5
Vinayak Goyal <73058928+ivinayakg@users.noreply.github.com>
2021-08-21 06:28:10 +0530

    completed the functioning of the app is added

--- src/App.js
+++ src/App.js
@@ -4,6 +4,43 @@ import { useState } from "react";
 
 export default function App() {
   const [msg, setMsg] = useState("");
+  const [init, setInit] = useState(0);
+  const [curr, setCurr] = useState(0);
+  const [qty, setQty] = useState(0);
+
+  const onchangeHandler = (e, value) => {
+    var data1 = e.target.value;
+    var data = parseInt(data1);
+    if (value === "initial") {
+      setInit(data);
+    } else if (value === "current") {
+      setCurr(data);
+    } else {
+      setQty(data);
+    }
+  };
+
+  const onclickHandler = (e) => {
+    e.preventDefault();
+    if (init > curr) {
+      var loss = (init - curr) * qty;
+      var percent = (loss / (init * qty)) * 100;
+      var result = `You Are in a loss of ${loss} and that is -${percent}%.Your Initial investment was ${
+        init * qty
+      }`;
+      setMsg(result);
+    }
+    if (init < curr) {
+      var profit = (curr - init) * qty;
+      var percent = (profit / (init * qty)) * 100;
+      var result = `You Are in a profit of ${profit} and that is +${percent}%.Your Initial investment was ${
+        init * qty
+      }`;
+      setMsg(result);
+    } else {
+      setMsg("No Pain No Gain!");
+    }
+  };
 
   return (
     <div className="App">
@@ -13,17 +50,31 @@ export default function App() {
           <div className="inputs">
             <label>
               Initial Price:
-              <input type="number" name="Initial Price" />
+              <input
+                type="number"
+                name="Initial Price"
+                onChange={(e) => onchangeHandler(e, "initial")}
+              />
             </label>
             <label>
               Current Price:
-              <input type="number" name="Current Price" />
+              <input
+                type="number"
+                name="Current Price"
+                onChange={(e) => onchangeHandler(e, "current")}
+              />
             </label>
             <label>
               Quantity:
-              <input type="number" name="Quantity" />
+              <input
+                type="number"
+                name="Quantity"
+                onChange={(e) => onchangeHandler(e, "quantity")}
+              />
             </label>
-            <button className="btn">Calculate</button>
+            <button className="btn" onClick={onclickHandler}>
+              Calculate
+            </button>
           </div>
           <h4>{msg}</h4>
         </div>
